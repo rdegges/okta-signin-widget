9711244cb575ed227c38419231d5142fd75f97ec
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-04-18 13:27:14 -0700

    Testing bacon

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
+export SAUCE_USERNAME=okta-qa
+export SAUCE_ACCESS_KEY="$(aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/saucelabs/saucelabs_access_key /dev/stdout)"
+export SAUCE_PLATFORM_NAME="iOS";
 
 setup_service google-chrome-stable 66.0.3359.139-1
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+cd ${OKTA_HOME}/${REPO}
 
-export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
-echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
-echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+setup_service grunt
 
-if ! npm test; then
-  echo "unit failed! Exiting..."
-  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+# Install required dependencies
+npm install -g @okta/ci-update-package
+npm install -g @okta/ci-pkginfo
+
+if ! npm install --no-optional --unsafe-perm; then
+  echo "npm install failed! Exiting..."
+  exit ${FAILED_SETUP}
 fi
 
-exit $PUBLISH_TYPE_AND_RESULT_DIR;
+function update_yarn_locks() {
+    git checkout -- test/e2e/react-app/yarn.lock
+    git checkout -- test/e2e/angular-app/yarn.lock
+
+    YARN_REGISTRY=https://registry.yarnpkg.com
+    OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
+
+    # Yarn does not utilize the npmrc/yarnrc registry configuration
+    # if a lockfile is present. This results in `yarn install` problems
+    # for private registries. Until yarn@2.0.0 is released, this is our current
+    # workaround.
+    #
+    # Related issues:
+    #  - https://github.com/yarnpkg/yarn/issues/5892
+    #  - https://github.com/yarnpkg/yarn/issues/3330
+
+    # Replace yarn artifactory with Okta's
+    sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" test/e2e/react-app/yarn.lock
+    sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" test/e2e/angular-app/yarn.lock
+}
+
+
+update_yarn_locks
+
+if ! npm run test:e2e; then
+  echo "e2e tests on iOS failed! Exiting..."
+  exit ${FAILURE}
+fi
+
+exit ${SUCCESS}
